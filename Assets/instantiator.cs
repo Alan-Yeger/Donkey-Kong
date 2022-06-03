0e4a1a5ad28e739b855c3a97676a3d5dc17e782b
Alan-Yeger <alanyeger1@gmail.com>
2022-06-03 00:29:12 -0300

    Initial commit

---
+++ Assets/instantiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiator : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject ball2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball);
+        Instantiate(ball2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
